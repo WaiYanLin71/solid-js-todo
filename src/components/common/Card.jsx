077432319495f8fa9dba4d232917821a16557bca
WaiYanLin71 <scm.waiyanlinn@gmail.com>
2023-02-01 15:11:21 +0630

    fixed width

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ children }) => {
     return (
-        <div class='max-w-md w-full p-6 bg-white border border-gray-200 rounded-lg shadow'>{children}</div>
+        <div class='max-w-lg w-full p-6 bg-white border border-gray-200 rounded-lg shadow'>{children}</div>
     )
 }
 
